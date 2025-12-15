bcf4dcb176fbf52d09f7cbaa000e11724b3abd19
Adhitya Ravi <adhitya.ravi@canonical.com>
2025-12-15 14:27:01 +0100

    feat: add AuthorizationPolcy as a custom lightkube resource

--- src/lightkube_extensions/types/__init__.py
+++ src/lightkube_extensions/types/__init__.py
@@ -1,12 +1,20 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Helpers for mocking and testing with Lightkube."""
+"""Type definitions and custom resource types for Lightkube."""
 
 from ._aggregate_types import (
     LightkubeResourcesList,
     LightkubeResourceType,
     LightkubeResourceTypesSet,
 )
+from ._custom_resource_types import (
+    AuthorizationPolicy,
+)
 
-__all__ = [LightkubeResourcesList, LightkubeResourceType, LightkubeResourceTypesSet]
+__all__ = [
+    "LightkubeResourcesList",
+    "LightkubeResourceType",
+    "LightkubeResourceTypesSet",
+    "AuthorizationPolicy",
+]
